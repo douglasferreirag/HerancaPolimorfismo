eba113e8c10c14cf5655a620ea133a56e4bab096
Douglas <douglas.ferreirag@gmail.com>
2022-10-11 23:15:11 -0300

    Herança

---
+++ Models/Aluno.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerancaPolimorfismo.Models
+{
+    public class Aluno : Pessoa
+    {
+        
+            public double Nota { get; set; }
+
+    }
+}
